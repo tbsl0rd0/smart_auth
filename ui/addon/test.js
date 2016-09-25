bc426c0dcedfda1d9cc972e19126cd67020f5a41
phantombm <phantombm@naver.com>
2016-09-25 22:16:05 +0900

    버전관리를 하지 않을 파일들을 삭제

--- ui/addon/test.js
+++ ui/addon/test.js
@@ -1,4 +1,4 @@
 const addon = require('./build/Release/addon');
 
-console.log(addon.get_current_user_name());
-console.log(addon.hide_other_users());
+console.log(addon.set_current_user_name_to_registry());
+// console.log(addon.hide_other_users());
