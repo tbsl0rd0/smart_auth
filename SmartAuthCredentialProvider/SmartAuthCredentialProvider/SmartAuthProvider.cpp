0b159ce721ac568942cb7dc506ccb1460d421ea3
KHS\홍선 <hongsun571@naver.com>
2016-10-29 16:34:10 +0900

    재변경 - CP 잠금화면 로드

--- SmartAuthCredentialProvider/SmartAuthCredentialProvider/SmartAuthProvider.cpp
+++ SmartAuthCredentialProvider/SmartAuthCredentialProvider/SmartAuthProvider.cpp
@@ -123,6 +123,7 @@ HRESULT SmartAuthProvider::SetUsageScenario(
 	{
 	case CPUS_LOGON:
 	case CPUS_CREDUI:
+	case CPUS_UNLOCK_WORKSTATION:
 		_cpus = cpus;
 		
 		if (rf.REG_HardwareAuth == '1') {
@@ -231,7 +232,6 @@ HRESULT SmartAuthProvider::SetUsageScenario(
 		}
 		break;
 
-	case CPUS_UNLOCK_WORKSTATION:
 	case CPUS_CHANGE_PASSWORD:
 	     hr = E_NOTIMPL;
 		 break;
