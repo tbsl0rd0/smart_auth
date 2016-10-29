fe2f13b31653b1d2984d73a98cc22d956eb0e335
phantombm <phantombm@naver.com>
2016-10-29 17:53:36 +0900

    c/c++ addon의 rebuild 버전을 nw-builder의 버전과 일치시킴

--- ui/addon/test.js
+++ ui/addon/test.js
@@ -1,4 +1,3 @@
 const addon = require('./build/Release/addon');
 
-console.log(addon.set_current_user_name_to_registry());
-// console.log(addon.hide_other_users());
+console.log(addon.set_current_user_name());
